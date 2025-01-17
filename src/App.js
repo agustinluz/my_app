3b4424b4dcef68ed4483e44b68f7016a0b3d4f07
agustinluz <agustinluzmartinll@gmail.com>
2025-01-17 10:24:56 +0100

    POTENTECHURRI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import HelloComponent from './components/HelloComponent';
+import InputComponent from './components/InputComponent';
+import React, { Component } from 'react';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state={
+      name:'Agustin'
+    }
+  }
+  changeName=(event)=>{
+    this.setState({
+      name: event.target.value
+    })
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <HelloComponent nombre= {this.state.name}></HelloComponent>
+          <InputComponent nombre={this.state.name} cambiarNombre={this.changeName}></InputComponent>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
